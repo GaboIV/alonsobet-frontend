482c4e3bee538e13244850969fea8ddabbd6aa51
Gabriel Caraballo Moya <gcaraballoperuapps@gmail.com>
2022-04-12 21:46:57 -0500

    add: test game

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,18 +4,12 @@ import { AuthGuard } from './modules/auth/_services/auth.guard';
 
 export const routes: Routes = [
   {
-    path: 'auth',
-    loadChildren: () =>
-      import('./modules/auth/auth.module').then((m) => m.AuthModule),
-  },
-  {
     path: 'error',
     loadChildren: () =>
       import('./modules/errors/errors.module').then((m) => m.ErrorsModule),
   },
   {
     path: '',
-    canActivate: [AuthGuard],
     loadChildren: () =>
       import('./pages/layout.module').then((m) => m.LayoutModule),
   },
